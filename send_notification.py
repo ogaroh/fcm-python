18798805a325d03eaa0e69380b27671d2c4d2f70
Erick Ogaro <ogaroerick1998@gmail.com>
2021-10-13 15:19:05 +0300

    additional info

--- send_notification.py
+++ send_notification.py
@@ -1,9 +1,12 @@
 import fcm_manager as fcm
 
 
-# These fcm tokens come from the client side FCM SDKs.
-# In our case we'll get them from the Flutter app associate them with a user account
-# Then we'll query the token(s) from our database
+# These fcm tokens are generated from the client side FCM SDKs
+# In our case we'll get them from the Flutter app then associate them with a user account by
+# updating the user details and adding the device token, and the generation timestamp
+# to their account everytime they launch the app
+# Then we'll query the token(s) from our database any time we want to send a push notification
+
 fcm_tokens = [
     "ea4OEUB1SvadeJaO7qWVXK:APA91bFIFCs2XgzvVyEw3xaBqslRB3YrpUbhtNtzSgPQnZ5fvhwM0zDjn4NfmuyjhtDOhaLjEgfHcBhWP2hgILXiQi_5eK__tnKpqDJHBveasCSyA-B4ul5qErx6uQ53wDRAkWvh21cw"]
 
