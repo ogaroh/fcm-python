d3a6df4b7d6aee96915bb503b378cb64036703cd
Erick Ogaro <ogaroerick1998@gmail.com>
2021-10-13 15:08:32 +0300

    fcm_tokens rename

--- fcm_manager.py
+++ fcm_manager.py
@@ -1,14 +1,3 @@
-# Author: Erick Ogaro, 2021
-# Steps to run:
-#   1. Download the service key (JSON) for your FCM project in Firebase
-#   2. Run `pip install firebase-admin` in your project's virtual env
-#   3. Replace certificate path with the path to your downloaded service key JSON file
-#   4. Replace the `registration_tokens` list with your own from a server or something
-#   5. Run `python send_notification.py` to call the `send_push_notification()` function, passing in your custom
-#       title, body, the list of tokens (can have one) and the screen you want the notification to redirect
-#       the recipient to.
-#
-
 import firebase_admin
 from firebase_admin import credentials, messaging
 
--- send_notification.py
+++ send_notification.py
@@ -3,7 +3,7 @@ import fcm_manager as fcm
 
 # These registration tokens comes from the client side FCM SDKs.
 # In our case we'll get them from the Flutter app associate them with a user account
-registration_tokens = [
+fcm_tokens = [
     "ea4OEUB1SvadeJaO7qWVXK:APA91bFIFCs2XgzvVyEw3xaBqslRB3YrpUbhtNtzSgPQnZ5fvhwM0zDjn4NfmuyjhtDOhaLjEgfHcBhWP2hgILXiQi_5eK__tnKpqDJHBveasCSyA-B4ul5qErx6uQ53wDRAkWvh21cw"]
 
 
@@ -12,4 +12,4 @@ title = input("Enter notification title: ")
 body = input("Enter notification body: ")
 screen = input("Enter notification screen: ")
 
-fcm.send_push_notification(title, body, screen, registration_tokens)
+fcm.send_push_notification(title, body, screen, fcm_tokens)
