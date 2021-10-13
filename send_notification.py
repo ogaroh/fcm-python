4ee26d3e475d344ebdf84c9aab2e7dc627297a71
Erick Ogaro <ogaroerick1998@gmail.com>
2021-10-13 15:15:05 +0300

    additional info

--- send_notification.py
+++ send_notification.py
@@ -1,8 +1,9 @@
 import fcm_manager as fcm
 
 
-# These registration tokens comes from the client side FCM SDKs.
+# These fcm tokens come from the client side FCM SDKs.
 # In our case we'll get them from the Flutter app associate them with a user account
+# Then we'll query the token(s) from our database
 fcm_tokens = [
     "ea4OEUB1SvadeJaO7qWVXK:APA91bFIFCs2XgzvVyEw3xaBqslRB3YrpUbhtNtzSgPQnZ5fvhwM0zDjn4NfmuyjhtDOhaLjEgfHcBhWP2hgILXiQi_5eK__tnKpqDJHBveasCSyA-B4ul5qErx6uQ53wDRAkWvh21cw"]
 
